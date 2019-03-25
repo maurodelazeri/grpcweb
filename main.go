fda58476877608f8abd8a2e25a7d762b7cf30c71
Mauro Delazeri <maurodelazeri@gmail.com>
2019-03-25 17:49:48 -0400

    new update

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 }
 
 func allowCors(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
 	w.Header().Set("Access-Control-Expose-Headers", "grpc-status, grpc-message")
 	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, XMLHttpRequest, x-user-agent, x-grpc-web, grpc-status, grpc-message")
